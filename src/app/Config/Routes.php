45a8e6e1ceb96ca5bd6e3eef17fb0b37706b0182
Nikolaos Pispas <npispas@gmail.com>
2025-01-30 21:14:30 +0200

    feat(routes): updated the web routes
    - Updated the web routes and added all the user CRUD routes.

--- src/app/Config/Routes.php
+++ src/app/Config/Routes.php
@@ -5,3 +5,10 @@ use App\Core\Router;
 $router = Router::getInstance();
 
 $router->get('/login', 'Auth@login');
+
+$router->get('/users', 'User@index');
+$router->get('/users/create', 'User@create');
+$router->post('/users', 'User@store');
+$router->get('/users/{id}/edit', 'User@edit');
+$router->post('/users/{id}', 'User@update');
+$router->get('/users/{id}/delete', 'User@delete');
