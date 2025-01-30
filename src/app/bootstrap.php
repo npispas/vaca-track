48d9e251fca88a8a090a0b342e493513e29fa850
Nikolaos Pispas <npispas@gmail.com>
2025-01-30 20:59:18 +0200

    refactor(bootstrap): added the composer autoload on the boot up lifecycle

--- src/app/bootstrap.php
+++ src/app/bootstrap.php
@@ -1,5 +1,5 @@
 <?php
-
+require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/Core/Config.php';
 require_once __DIR__ . '/Core/Database.php';
 require_once __DIR__ . '/Config/Routes.php';
--- src/public/index.php
+++ src/public/index.php
@@ -1,6 +1,5 @@
 <?php
 
-require_once '../vendor/autoload.php';
 require_once '../app/bootstrap.php';
 
 use App\Core\Router;
