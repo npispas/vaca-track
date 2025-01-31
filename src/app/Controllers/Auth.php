0121b83375d898b49f757e6884663efce743fce3
Nikolaos Pispas <npispas@gmail.com>
2025-01-31 15:16:04 +0200

    feat(user auth): implementation of user authentication
    - Updated the auth controller to support user auth actions.
    - Updated the login view with proper feedback messages and form.
    - Added a separate layout for the auth flow.

--- src/app/Controllers/Auth.php
+++ src/app/Controllers/Auth.php
@@ -2,12 +2,47 @@
 
 namespace App\Controllers;
 
+use App\Core\Auth as AuthProvider;
+use App\Core\Controller;
+use App\Core\Request;
+use App\Core\Response;
 use App\Core\View;
 
-class Auth
+class Auth extends Controller
 {
     public function login(): void
     {
-        View::render('login');
+        if (AuthProvider::check()) {
+            Response::redirect('/')->send();
+        }
+
+        View::render('auth/login');
+    }
+
+    public function processLogin(): void
+    {
+        $errors = Request::validate([
+            'username' => ['required'],
+            'password' => ['required']
+        ]);
+
+        if ($errors) {
+            Response::redirect('/login')->with('errors', $errors)->send();
+        }
+
+        $data = Request::all();
+
+        if (AuthProvider::attempt($data['username'], $data['password'])) {
+            Response::redirect('/')->send();
+        }
+
+        Response::redirect('/login')->with('errors', ['User' => ['User not found!']])->send();
+    }
+
+    public function logout(): void
+    {
+        AuthProvider::logout();
+
+        Response::redirect('/login')->send();
     }
 }
