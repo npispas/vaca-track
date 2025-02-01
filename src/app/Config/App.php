c0e1edfc6e3495f47206da22a5311236a69d44cb
Nikolaos Pispas <npispas@gmail.com>
2025-02-01 22:24:06 +0200

    chore(env): added the vacation days env entry

--- src/app/Config/App.php
+++ src/app/Config/App.php
@@ -4,4 +4,5 @@ return [
     'debug' => getenv('APP_DEBUG') === 'true',
     'timezone' => getenv('APP_TIMEZONE') ?: 'UTC',
     'name' => getenv('APP_NAME') ?: 'VacaTrack',
+    'vacation_days' => getenv('APP_VACATION_DAYS') ?: 20,
 ];
