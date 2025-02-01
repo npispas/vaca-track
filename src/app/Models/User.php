ba264d1551e330d250b7a2834e2eebbb23cd5635
Nikolaos Pispas <npispas@gmail.com>
2025-02-01 22:25:01 +0200

    feat(user model): updated the user model with more business logic
    - Added the vacations functionality business logic in the user model.

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -2,11 +2,16 @@
 
 namespace App\Models;
 
+use App\Core\Config;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 /**
  * @method static find(int $id)
+ * @property int $id
+ * @property string $name
+ * @property int $remaining_vacation_days
  */
 class User extends Model
 {
@@ -21,6 +26,40 @@ class User extends Model
     protected $fillable = ['role_id', 'name', 'email', 'username', 'employee_id', 'password'];
 
     /**
+     * @var array The attributes that are appendable.
+     */
+    protected $appends = ['first_name', 'remaining_vacation_days'];
+
+    /**
+     * Calculates the user's first name.
+     *
+     * @return string
+     */
+    public function getFirstNameAttribute(): string
+    {
+        return trim(explode(' ', $this->name)[0]);
+    }
+
+    /**
+     * Calculates the user's remaining vacation days for the current year.
+     *
+     * @return int
+     */
+    public function getRemainingVacationDaysAttribute(): int
+    {
+        $totalDaysAllowed = Config::get('app')['vacation_days'];
+        $currentYear = date('Y');
+        $vacations = $this->vacations()
+            ->whereYear('start_date', $currentYear)
+            ->where('status', 'approved')
+            ->get();
+
+        $usedDays = $vacations->sum('duration');
+
+        return $totalDaysAllowed - $usedDays;
+    }
+
+    /**
      * Retrieves the user's associated role.
      *
      * @return BelongsTo
@@ -28,6 +67,16 @@ class User extends Model
     public function role(): BelongsTo
     {
         return $this->belongsTo(Role::class);
+    }
+
+    /**
+     * Retrieves the user's associated role.
+     *
+     * @return HasMany
+     */
+    public function vacations(): HasMany
+    {
+        return $this->hasMany(Vacation::class);
     }
 
     /**
